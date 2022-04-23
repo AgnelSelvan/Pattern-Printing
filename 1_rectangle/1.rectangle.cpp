9848d1aa3058bde65617d9046f518f4d0fa9e3b0
Agnel Selvan Arunachalam <agnel.selvan@neosoftmail.com>
2022-04-23 20:24:26 +0530

    Organized Folder Structure

---
+++ 1_rectangle/1.rectangle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    #ifndef READ_AND_WRITE_OP
+        freopen("../input.txt", "r", stdin);
+        freopen("../output.txt", "w", stdout);
+    #endif
+    int row, col;
+    cin>>row>>col;
+
+    cout << "Row " << row << endl;
+    cout << "Column " << col << endl << endl;
+    
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 1; j <= col; j++)
+        {
+            cout << "*";
+        }
+        cout<<endl;
+    }
+    
+}
